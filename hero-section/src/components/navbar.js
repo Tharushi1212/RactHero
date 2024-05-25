472fbf4b8e7b8f0eacfeb6edc206012fd2fdc0e1
Tharushi1212 <it20000608@my.sliit.lk>
2024-05-25 16:07:37 +0530

    #commit feature_header

--- hero-section/src/components/navbar.js
+++ hero-section/src/components/navbar.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import '../assets/app.css';
+
+import location from '../assets/images/location.png';
+import facebook from '../assets/images/facebook.png';
+import insta from '../assets/images/insta.png';
+import phone from '../assets/images/phone-icon.png';
+import xvector from '../assets/images/xvector.png';
+import printerest from '../assets/images/Pinterest.png';
 
 const Navbar = () => {
   return (
@@ -8,40 +14,16 @@ const Navbar = () => {
         <div class="inner-container">
           <div class="d-flex justify-content-between">
             <div class="header-contact-wrapper">
-              <img
-                src="./images/home/location.png"
-                alt="location"
-                class="location-icon"
-              />
+              <img src={location} alt="location" class="location-icon" />
               <p class="header-text-location">Central Africa, Angola</p>
-              <img
-                src="./images/home/phone-icon.png"
-                alt="location"
-                class="location-icon"
-              />
+              <img src={phone} alt="location" class="location-icon" />
               <p class="header-text-location">+244 1234 7890</p>
             </div>
             <div class="header-icon-wrapper">
-              <img
-                src="./images/home/insta.png"
-                alt="location"
-                class="location-icon"
-              />
-              <img
-                src="./images/home/facebook.png"
-                alt="location"
-                class="location-icon"
-              />
-              <img
-                src="./images/home/xvector.png"
-                alt="location"
-                class="location-icon"
-              />
-              <img
-                src="./images/home/Pinterest.png"
-                alt="location"
-                class="location-icon"
-              />
+              <img src={insta} alt="location" class="location-icon" />
+              <img src={facebook} alt="location" class="location-icon" />
+              <img src={xvector} alt="location" class="location-icon" />
+              <img src={printerest} alt="location" class="location-icon" />
             </div>
           </div>
         </div>
